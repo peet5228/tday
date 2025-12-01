ffa80fc9aa9823776f7a8c7f8cb84a6b61292edb
peet5228 <ppeet5228@gmail.com>
2025-12-01 01:57:40 -0800

    set project peet

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const express = require('express')
+const fileupload = require('express-fileupload')
+const cors = require('cors')
+const path = require('path')
+const app = express()
+
+app.use(cors ({
+    origin:'http://localhost:5173',
+    credentials:true
+}))
+
+app.use(express.json())
+
+app.listen(3001 , () => console.log('Server Running On Port 3001'))
