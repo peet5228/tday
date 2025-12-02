2d8cf2e0d4549030e1ad16b4f47b1c7a20eab6a0
peet5228 <ppeet5228@gmail.com>
2025-12-01 18:35:15 -0800

    Login

--- backend/index.js
+++ backend/index.js
@@ -11,5 +11,14 @@ app.use(cors ({
 }))
 
 app.use(express.json())
+app.use(fileupload())
+app.use(('/uploads',express.static(path.join('uploads'))))
+
+const auth = require('./routes/auth')
+app.use('/api/auth',auth)
+
+
+// 404
+app.use((req,res) => res.status(404).json({message:'ปิดปรับปรุง'}))
 
 app.listen(3001 , () => console.log('Server Running On Port 3001'))
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginForm from '../LoginForm.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
+    {
+      path: '/login',
+      name: 'LoginForm',
+      component: LoginForm,
+    },
+    {
+      path: '/',
+      redirect: '/login',
+    },
   ],
 })
 
